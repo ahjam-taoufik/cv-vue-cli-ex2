27837c89e260752cf06409d6f7758a9e7f5cd658
taoufik ahjam <taoufikahjam@gmail.com>
2022-06-25 21:13:01 +0200

    first component

---
+++ src/components/Contenu.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+       {{txt}}
+    </div>
+</template>
+
+<script>
+    export default {
+        data(){
+            return {
+                txt:'Votre Contenu IcI'
+            }
+        }
+    }
+
+    
+</script>
+
+<style scoped>
+
+</style>
