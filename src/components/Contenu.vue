3ebd3af29c6a0f00768a8b24a0a1a8b727fe88c1
taoufik ahjam <taoufikahjam@gmail.com>
2022-06-26 00:30:50 +0200

    value by reference vs Primitive

--- src/components/Contenu.vue
+++ src/components/Contenu.vue
@@ -1,10 +1,7 @@
 <template>
   <div class="container mt-5">
-    <list v-bind:films="films1" ></list>
-     <button v-on:click="deleteFilm" class="btn btn-primary">Delete One Film</button>
-     
-    <list v-bind:films="films1" ></list>
-     <button v-on:click="deleteFilm" class="btn btn-primary">Delete One Film</button>
+    <list v-bind:films="films1" v-bind:txt="txt1"></list>
+    <list v-bind:films="films1" v-bind:txt="txt1"></list>
   </div>
 </template>
 
@@ -13,23 +10,17 @@ import List from './list/List.vue';
 export default {
   data() {
     return {
-      txt: 'Votre Contenu IcI',
-      films1:[
-        {name:'gladiator', year:2000},
-        {name:'seven', year:1999},
-        {name:'taken', year:2010}
+      films1: [
+        { name: 'gladiator', year: 2000 },
+        { name: 'seven', year: 1999 },
+        { name: 'taken', year: 2010 },
       ],
-      
+      txt1: 'Votre Contenu IcI',
     };
   },
   components: {
     list: List,
   },
-  methods: {
-    deleteFilm:function(){
-        this.films1.pop()
-    }
-  }
 };
 </script>
 
--- src/components/list/List.vue
+++ src/components/list/List.vue
@@ -5,23 +5,34 @@
         <div class="card">
           <div class="card-body">
             name : {{ film.name }}
-            <br/>
+            <br />
             year : {{ film.year }}
           </div>
         </div>
       </li>
     </ul>
+    <p>{{ txt }}</p>
+      <button v-on:click="deleteFilm" class="btn btn-primary">Delete One Film</button>
+     <button v-on:click="deleteTxt" class="btn btn-primary">Delete txt</button>
   </div>
 </template>
 
 <script>
 export default {
   data() {
-    return {
-     
-    };
+    return {};
   },
-  props: ['films'],
+  methods: {
+    deleteFilm: function () {
+     // eslint-disable-next-line vue/no-mutating-props
+     this.films.pop()
+    },
+    deleteTxt: function () {
+      // eslint-disable-next-line vue/no-mutating-props
+      this.txt = '';
+    },
+  },
+  props: ['films', 'txt'],
 };
 </script>
 
