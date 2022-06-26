14d145943f0f4fd9cae5a00a6dea9a2ace13b6a2
taoufik ahjam <taoufikahjam@gmail.com>
2022-06-26 21:45:59 +0200

    link dynamic

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,9 +1,9 @@
 <template>
     <div>
-        <ul>
-            <li><router-link to="/" exact>Home</router-link></li>
-            <li><router-link to="/p1">page1</router-link></li>
-            <li><router-link to="/p2">page2</router-link></li>
+        <ul class="nav">
+            <li class="btn btn-primary btn-custom"><router-link to="/" exact>Home</router-link></li>
+            <li class="btn btn-primary btn-custom"><router-link to="/p1">page1</router-link></li>
+            <li class="btn btn-primary btn-custom"><router-link to="/p2">page2</router-link></li>
         </ul>
         
 
@@ -17,9 +17,13 @@
 </script>
 
 <style  scoped>
- .router-link-active{
-    color:rgb(221, 176, 27);
-    background:rgb(248, 237, 87)
- }
+  .btn-custom {
+    margin: 20px;
+    color:aliceblue
+  }
+  .btn-custom a{
+    margin: 20px;
+    color:aliceblue
+  }
 
 </style>
--- src/components/Page1.vue
+++ src/components/Page1.vue
@@ -1,12 +1,19 @@
 <template>
     <div>
        <h1>this is  page 1</h1>
+      <button v-on:click="retour" class="btn btn-warning">Retour au home page</button> </button>
+
     </div>
 </template>
 
 <script>
     export default {
-           name:'Page1'
+           name:'Page1',
+    methods:{
+        retour:function(){
+            this.$router.push('/')
+        }
+    }
         
     }
 </script>
