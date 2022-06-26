2455ee47287191d1cdd32dc168f0afbe81e2b2ff
taoufik ahjam <taoufikahjam@gmail.com>
2022-06-26 21:29:48 +0200

    router link

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,17 @@
 <template>
   <div id="app">
+    <header-top></header-top>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
-  
+  import Header from './components/Header.vue'
 
 export default {
   name: 'App',
   components: {
- 
+     'header-top':Header
   
   }
 }
---
+++ src/components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <ul>
+            <li><router-link to="/" exact>Home</router-link></li>
+            <li><router-link to="/p1">page1</router-link></li>
+            <li><router-link to="/p2">page2</router-link></li>
+        </ul>
+        
+
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style  scoped>
+ .router-link-active{
+    color:rgb(221, 176, 27);
+    background:rgb(248, 237, 87)
+ }
+
+</style>
